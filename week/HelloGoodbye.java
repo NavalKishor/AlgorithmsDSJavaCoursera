e4140fc8c99962edb597ed46eaeebef05d5aee28
Naval Kishor Jha <er.navalkishor@yahoo.in>
2021-08-13 17:25:16 +0530

    hellogoodbye program in java with 2 param

--- week/HelloGoodbye.java
+++ week/HelloGoodbye.java
@@ -4,25 +4,17 @@
 public class HelloGoodbye {
 
     public static void main(String[] args) {
-    	int noOfparam=args.length;
-    	switch(noOfparam){
-    	    case 0:
-    			System.out.println("Hello can you pass your name please!");
-    			break;
-    		case 1:
-    			String param1=args[0];
-    			System.out.println("Hello "+param1);
-    			System.out.println("Goodbye "+param1);
-    			break;
-    	    default:
-    	        if(noOfparam>=2){
-    		        param1=args[0];
-				    String param2=args[1];
-        		    System.out.println("Hello "+param1+" and "+param2);
-        		    System.out.println("Goodbye "+param1+" and "+param2);
-    		    } else {
-    		        System.out.println("Hello -ve value Exception case fault in system");
-    		    }
+    	 
+    	if(args.length>=2)
+    	{
+    		String param1=args[0];
+		    String param2=args[1];    	
+		    System.out.println("Hello "+param1+" and "+param2);
+		    System.out.println("Goodbye "+param1+" and "+param2);
+		} 
+		else 
+		{
+			System.out.println("Hello pass at least two name please.");
     	}
     	
     }
